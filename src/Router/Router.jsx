3c32e4f617f42a90a31221c1aeac0ce23b3049aa
soikotdeb <sdsoikot424@gmail.com>
2023-08-24 14:07:33 +0600

    error Page added to the project

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import NoFound from "../Pages/Shared/ErrorPage/NoFound";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element:<Main></Main>
+      element:<Main></Main>,
+      errorElement:<NoFound></NoFound>,
+      children:[
+        
+      ]
     },
   ]);
